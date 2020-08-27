33e10b48a748b029ecc995be1935e070c32296af
saidbouig <saidbouigherdaine@gmail.com>
2020-08-27 19:50:09 +0200

    add business logic to service-user

---
+++ service-user/src/main/java/com/saidbouig/serviceuser/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.saidbouig.serviceuser.exception;
+
+public class ResourceNotFoundException extends Exception{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ service-user/src/main/java/com/saidbouig/serviceuser/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.saidbouig.serviceuser.repository;
+
+import com.saidbouig.serviceuser.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {}
+
