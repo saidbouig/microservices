dd4bdbeca47b908a34fc6ab1537ff7a814042c4d
saidbouig <saidbouigherdaine@gmail.com>
2021-06-22 12:56:50 +0200

    introduce userService

---
+++ service-user/src/main/java/com/saidbouig/serviceuser/domain/UserService.java
@@ -0,0 +1,49 @@
+package com.saidbouig.serviceuser.domain;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UsersRepository usersRepository;
+
+    public UserService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public User getById(Long userId) throws ResourceNotFoundException {
+        return usersRepository
+                .findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found on :: " + userId));
+    }
+
+    public List<User> getAll() {
+        return usersRepository.findAll();
+    }
+
+    public User create(User user) {
+        return usersRepository.save(user);
+    }
+
+    public User update(Long userId, User userDetails) throws ResourceNotFoundException {
+        User user = usersRepository
+                .findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found on :: " + userId));
+
+        user.setEmail(userDetails.getEmail());
+        user.setLastName(userDetails.getLastName());
+        user.setFirstName(userDetails.getFirstName());
+        user.setUpdatedAt(new Date());
+
+        return user;
+    }
+
+    public void delete(Long userId) throws ResourceNotFoundException {
+        User user =
+                getById(userId);
+
+        usersRepository.delete(user);
+    }
+}
