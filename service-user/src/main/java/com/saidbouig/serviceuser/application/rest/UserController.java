c94b21bf0e6477c4b8618d7a9713dbf353d2b330
saidbouig <saidbouigherdaine@gmail.com>
2021-06-22 16:28:21 +0200

    export api version to bootstrap.properties

--- service-user/src/main/java/com/saidbouig/serviceuser/application/rest/UserController.java
+++ service-user/src/main/java/com/saidbouig/serviceuser/application/rest/UserController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("users")
 public class UserController {
     private final UserService userService;
 
